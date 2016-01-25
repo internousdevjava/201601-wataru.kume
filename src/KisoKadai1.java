b6ccbeedfadad761892e7ed29b560978848f6a5a
Wataru Kume <km42.twfno@gmail.com>
2016-01-25 16:08:38 +0900

    最大値の桁数を参照し、現状の計算結果に対して足りない桁数分空白を挿入するよう修正いたしました。 また、桁数が自由に入力出来なかったため、併せて修正いたしました。

--- src/KisoKadai1.java
+++ src/KisoKadai1.java
@@ -8,20 +8,30 @@
  */
 public class KisoKadai1 {
 			//九九表作成課題
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
 		System.out.println("九九の表");
+		System.out.println("最大値xを入力して下さい。");
+		int input1 = new java.util.Scanner (System.in).nextInt();
+		System.out.println("最大値yを入力して下さい。");
+		int input2 = new java.util.Scanner (System.in).nextInt();
 
-		int x=9,y=9,i,j;
+		int x = input1;
+		int y = input2;
+
+		int i,j;
 			//最大値①,最大値②,最小値①,最小値②
+		int a = String.valueOf(x*y).length();
 		for(i=1; i<=x; i++){
 				for (j=1; j<=y; j++) {
-					System.out.print(i*j);
-					System.out.print(" ");
+					int b = String.valueOf(i*j).length();
+					if(a-b == 0){
+						System.out.print(i*j+" ");
+					}else{
+						for(int c=1; c<=a-b; c++){
+							System.out.print(" ");
+						}
+						System.out.print(i*j+" ");
+					}
 				}
 			System.out.println("");
 		}
